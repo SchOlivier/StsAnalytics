eb54b81c713c70ea98ae525180220ce7b0495164
Virgil <virgildoerr@gmail.com>
2023-02-26 16:24:43 +0100

    Change Fight

--- src/Entity/room/Fight.php
+++ src/Entity/room/Fight.php
@@ -1,10 +1,20 @@
 <?php
+
 namespace App\Entity\room;
+
+use App\Entity\ref\Encounter;
 
 class Fight implements IRoom
 {
     // private Encounter $encounter;
-    private int $nbTurn;
+
+
+    public function __construct(
+        private int $nbTurn,
+        private int $damageTaken,
+        private Encounter $encounter
+    ) {
+    }
 
     public function getName(): string
     {
